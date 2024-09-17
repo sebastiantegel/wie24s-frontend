5ffc9a1ac2c9c7c57df07bcd51c56dac37c5d026
Sebastian Tegel <sebastian.tegel@tegelconsulting.se>
2024-09-17 11:34:00 +0200

    :tada: Added project

---
+++ JS_Basics/main.js
@@ -0,0 +1,23 @@
+let age = 44;
+const name = "Sebastian";
+const isMarried = true;
+
+console.log("Hello world!");
+console.log("Age:", age);
+console.log("IsMarried:", isMarried);
+
+age = age + 1; // age -> 45
+
+console.log("New age:", age);
+
+age += 4; // age -> 49
+
+console.log("New age again:", age);
+
+age++; // age -> 50
+
+console.log("New age yet again:", age);
+
+let m = 10 % 2; // m -> 0
+
+console.log("Modulus", m);
